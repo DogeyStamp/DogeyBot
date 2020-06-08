bbd532de700aa1c2f1c37f237a7e1822214c3ebb
DogeyStamp <61116261+DogeyStamp@users.noreply.github.com>
2020-06-08 15:09:22 -0400

    Calculator added

--- bot.py
+++ bot.py
@@ -3,6 +3,9 @@ import markovify
 import random
 import dogeystrings
 import dogeycmds
+import sympy
+import multiprocessing
+import time
 
 client = discord.Client()
 
@@ -10,17 +13,36 @@ client = discord.Client()
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
+def calc(eq):
+    result = sympy.sympify(eq)
+    if len(str(result)) > 6000:
+        message.channel.send("aww. ur result is too chonker!!")
+        return
+    response = str(sympy.sympify(eq))
+    for chunk in [response[i:i+2000] for i in range(0, len(response), 2000)]:
+        message.channel.send(chunk)
+
 @client.event
 async def on_message(message):
-    if message.author == client.user or message.content.lower()[:5] != "bork ":
+    if message.author == client.user or message.content.lower()[:4] != "bork":
         return
     try:
-        if message.content.find("help") != -1 or message.content.find("halp") != -1:
+        if message.content.lower().find("help") != -1 or message.content.lower().find("halp") != -1:
             await message.channel.send(dogeystrings.helpPrompt)
             return
-        if message.content.find("commands") != -1:
+        if message.content.lower().find("commands") != -1:
             for cmd in dogeycmds.cmds.keys():
                 await message.channel.send(cmd + ": " + dogeycmds.cmds[cmd])
+            return
+        if message.content.lower().find("calculate") != -1 or message.content.lower().find("what's") != -1 or message.content.lower().find("whats") != -1:
+            whitelist = ["1","2","3","4","5","6","7","8","9","0","*","+","%","-","/","!","^","(",")"]
+            ind = max(message.content.lower().find("calculate"),message.content.lower().find("what's"),message.content.lower().find("whats"))
+            eq = ''.join(ch for ch in message.content.lower()[ind:] if ch in whitelist)
+            p = multiprocessing.Process(target=calc, name="Calc", args=(eq,))
+            p.start()
+            await asyncio.sleep(1)
+            p.terminate()
+            p.join()
             return
         with open("dogebase.txt",encoding="utf-8") as f:
             text = f.read().split("\n")
--- dogeycmds.py
+++ dogeycmds.py
@@ -1,2 +1,3 @@
 cmds = {"help":"Get basic help. Know how to talk to good doggos!",
-        "commands":"Detailed help for all commands."}
+        "commands":"Detailed help for all commands.",
+        "calculate":"Calculate an equation."}
