cb112057ad04fdc538a62b3be3c6ef0ff9992755
DogeyStamp <adeng.sean@gmail.com>
2020-06-08 00:05:35 -0400

    improved stuff

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord
 import markovify
 
-with open("doge.txt",encoding="utf-8") as f:
+with open("doge2.txt",encoding="utf-8") as f:
     text = f.read()
 text_model = markovify.Text(text)
 
@@ -16,10 +16,12 @@ async def on_message(message):
     if message.author == client.user or message.content.lower()[:5] != "bork ":
         return
     try:
-        response = text_model.make_sentence()
+        text_model = markovify.Text(text)
+        response = text_model.make_short_sentence(350)
         while not response:
-            response = text_model.make_sentence()
-        await message.channel.send(text_model.make_sentence())
+            response = text_model.make_short_sentence(350)
+        responseL = '!'.join(response.split('.')).split('!')
+        await message.channel.send('\n'.join(responseL[:3])+'\nif u want halp say bork bork i want help or something similar')
         
     except Exception as err:
         await message.channel.send("Error: {}".format(err))
